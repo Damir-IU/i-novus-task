a3f45635abe23b26c4667dd3cd1a57dc9910fab3
Damir-IU <YusupovDamir1490@yandex.ru>
2023-09-23 10:38:39 +0300

    [0] - init commit

---
+++ src/main/java/com/task/inovus/INovusApplication.java
@@ -0,0 +1,11 @@
+package com.task.inovus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class INovusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(INovusApplication.class, args);
+    }
+}
